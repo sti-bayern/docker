688345ffbcbd1f3bcbce3c423f0b93fc5aeba481
Ayhan Akilli <info@ayhan.biz>
2015-07-03 22:59:45 +0200

    Clean up

--- postgres.sh
+++ postgres.sh
@@ -3,8 +3,7 @@
 chown -R postgres:postgres /var/lib/postgresql
 chown -R postgres:postgres /run/postgresql
 
-if [ -z "$(ls -A /var/lib/postgresql)" ]
-then
+if [ -z "$(ls -A /var/lib/postgresql)" ]; then
     su postgres -c "mkdir -p /var/lib/postgresql/9.4/main"
     su postgres -c "/usr/lib/postgresql/9.4/bin/initdb -D /var/lib/postgresql/9.4/main -E UTF8"
     su postgres -c "/usr/lib/postgresql/9.4/bin/postgres --single --config-file=/etc/postgresql/9.4/main/postgresql.conf" <<< "ALTER USER postgres WITH PASSWORD '';" > /dev/null
