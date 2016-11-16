2fde30419b7e121e32342b8e13d178e64a2c08ce
Ayhan Akilli <info@ayhan.biz>
2016-11-17 00:32:06 +0100

    Clean up

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@ if [ -z "$(ls -A /var/lib/postgresql)" ]; then
     su postgres -c "/usr/lib/postgresql/$POSTGRES/bin/postgres --single --config-file=/etc/postgresql/$POSTGRES/main/postgresql.conf" <<< "ALTER USER postgres WITH PASSWORD '$POSTGRES_PWD';"
     su postgres -c "/usr/lib/postgresql/$POSTGRES/bin/postgres --single --config-file=/etc/postgresql/$POSTGRES/main/postgresql.conf" <<< "CREATE USER app WITH PASSWORD '$POSTGRES_APP_PWD';"
     su postgres -c "/usr/lib/postgresql/$POSTGRES/bin/postgres --single --config-file=/etc/postgresql/$POSTGRES/main/postgresql.conf" <<< "CREATE DATABASE app WITH OWNER app ENCODING 'UTF8';"
-    su postgres -c "/usr/lib/postgresql/$POSTGRES/bin/postgres --single --config-file=/etc/postgresql/$POSTGRES/main/postgresql.conf" <<< "GRANT ALL PRIVILEGES ON DATABASE app TO app;"
+    su postgres -c "/usr/lib/postgresql/$POSTGRES/bin/postgres --single --config-file=/etc/postgresql/$POSTGRES/main/postgresql.conf" <<< "GRANT ALL PRIVILEGES ON app TO app;"
 fi
 
 su postgres -c "/usr/lib/postgresql/$POSTGRES/bin/postgres --config-file=/etc/postgresql/$POSTGRES/main/postgresql.conf"
