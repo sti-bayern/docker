cc1a504a88d3929ae3af4e2b245f4df3958c708f
Ayhan Akilli <info@ayhan.biz>
2016-11-17 00:52:38 +0100

    Update postgres + mysql config

--- Dockerfile
+++ Dockerfile
@@ -6,17 +6,17 @@ MAINTAINER Ayhan Akilli
 # Build variables
 #
 ARG DEBIAN_FRONTEND=noninteractive
-ARG POSTGRES=9.6
-ARG POSTGRES_APP_PWD=app
-ARG POSTGRES_PWD=postgres
+ARG PG=9.6
+ARG PG_APP=app
+ARG PG_ROOT=postgres
 
 #
 # Environment variables
 #
-ENV PATH=/usr/lib/postgresql/$POSTGRES/bin:$PATH \
-    POSTGRES=$POSTGRES \
-    POSTGRES_APP_PWD=$POSTGRES_APP_PWD \
-    POSTGRES_PWD=$POSTGRES_PWD
+ENV PATH=/usr/lib/postgresql/$PG/bin:$PATH \
+    PG=$PG \
+    PG_APP=$PG_APP \
+    PG_ROOT=$PG_ROOT
 
 #
 # APT packages
@@ -25,21 +25,21 @@ RUN wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-k
     echo "deb http://apt.postgresql.org/pub/repos/apt/ xenial-pgdg main" > /etc/apt/sources.list.d/postgres.list
 
 RUN apt-get update && apt-get install -y \
-    postgresql-$POSTGRES \
-    postgresql-contrib-$POSTGRES
+    postgresql-$PG \
+    postgresql-contrib-$PG
 
 RUN rm -rf /var/lib/apt/lists/* && \
     rm -rf /var/lib/postgresql && \
     mkdir -p /var/lib/postgresql && \
-    mkdir -p /var/run/postgresql/$POSTGRES-main.pg_stat_tmp && \
-    chown postgres:postgres /var/run/postgresql/$POSTGRES-main.pg_stat_tmp
+    mkdir -p /var/run/postgresql/$PG-main.pg_stat_tmp && \
+    chown postgres:postgres /var/run/postgresql/$PG-main.pg_stat_tmp
 
 #
 # Configuration
 #
-RUN echo "listen_addresses='*'" >> /etc/postgresql/$POSTGRES/main/postgresql.conf
+RUN echo "listen_addresses='*'" >> /etc/postgresql/$PG/main/postgresql.conf
 
-COPY pg_hba.conf /etc/postgresql/$POSTGRES/main/pg_hba.conf
+COPY pg_hba.conf /etc/postgresql/$PG/main/pg_hba.conf
 
 #
 # Volumes
--- entrypoint.sh
+++ entrypoint.sh
@@ -6,12 +6,12 @@ chown -R postgres:postgres /var/lib/postgresql
 chown -R postgres:postgres /run/postgresql
 
 if [ -z "$(ls -A /var/lib/postgresql)" ]; then
-    su postgres -c "mkdir -p /var/lib/postgresql/$POSTGRES/main"
-    su postgres -c "/usr/lib/postgresql/$POSTGRES/bin/initdb -D /var/lib/postgresql/$POSTGRES/main -E UTF8"
-    su postgres -c "/usr/lib/postgresql/$POSTGRES/bin/postgres --single --config-file=/etc/postgresql/$POSTGRES/main/postgresql.conf" <<< "ALTER USER postgres WITH PASSWORD '$POSTGRES_PWD';"
-    su postgres -c "/usr/lib/postgresql/$POSTGRES/bin/postgres --single --config-file=/etc/postgresql/$POSTGRES/main/postgresql.conf" <<< "CREATE USER app WITH PASSWORD '$POSTGRES_APP_PWD';"
-    su postgres -c "/usr/lib/postgresql/$POSTGRES/bin/postgres --single --config-file=/etc/postgresql/$POSTGRES/main/postgresql.conf" <<< "CREATE DATABASE app WITH OWNER app ENCODING 'UTF8';"
-    su postgres -c "/usr/lib/postgresql/$POSTGRES/bin/postgres --single --config-file=/etc/postgresql/$POSTGRES/main/postgresql.conf" <<< "GRANT ALL PRIVILEGES ON app TO app;"
+    su postgres -c "mkdir -p /var/lib/postgresql/$PG/main"
+    su postgres -c "/usr/lib/postgresql/$PG/bin/initdb -D /var/lib/postgresql/$PG/main -E UTF8"
+    su postgres -c "/usr/lib/postgresql/$PG/bin/postgres --single --config-file=/etc/postgresql/$PG/main/postgresql.conf" <<< "ALTER USER postgres WITH PASSWORD '$PG_ROOT';"
+    su postgres -c "/usr/lib/postgresql/$PG/bin/postgres --single --config-file=/etc/postgresql/$PG/main/postgresql.conf" <<< "CREATE USER app WITH PASSWORD '$PG_APP';"
+    su postgres -c "/usr/lib/postgresql/$PG/bin/postgres --single --config-file=/etc/postgresql/$PG/main/postgresql.conf" <<< "CREATE DATABASE app WITH OWNER app ENCODING 'UTF8';"
+    su postgres -c "/usr/lib/postgresql/$PG/bin/postgres --single --config-file=/etc/postgresql/$PG/main/postgresql.conf" <<< "GRANT ALL PRIVILEGES ON app TO app;"
 fi
 
-su postgres -c "/usr/lib/postgresql/$POSTGRES/bin/postgres --config-file=/etc/postgresql/$POSTGRES/main/postgresql.conf"
+su postgres -c "/usr/lib/postgresql/$PG/bin/postgres --config-file=/etc/postgresql/$PG/main/postgresql.conf"
