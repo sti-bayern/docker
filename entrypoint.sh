a6063bf127cad14c182a85c41a80aac239dd0773
Ayhan Akilli <info@ayhan.biz>
2016-11-29 14:51:48 +0100

    Turn app to superuser for now

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ if [ -z "$(ls -A /var/lib/postgresql)" ]; then
     su postgres -c "mkdir -p /var/lib/postgresql/$PG/main"
     su postgres -c "/usr/lib/postgresql/$PG/bin/initdb -D /var/lib/postgresql/$PG/main -E UTF8"
     su postgres -c "/usr/lib/postgresql/$PG/bin/postgres --single --config-file=/etc/postgresql/$PG/main/postgresql.conf" <<< "ALTER USER postgres WITH PASSWORD '$PG_ROOT';"
-    su postgres -c "/usr/lib/postgresql/$PG/bin/postgres --single --config-file=/etc/postgresql/$PG/main/postgresql.conf" <<< "CREATE USER app WITH PASSWORD '$PG_APP';"
+    su postgres -c "/usr/lib/postgresql/$PG/bin/postgres --single --config-file=/etc/postgresql/$PG/main/postgresql.conf" <<< "CREATE USER app WITH PASSWORD '$PG_APP' SUPERUSER;"
     su postgres -c "/usr/lib/postgresql/$PG/bin/postgres --single --config-file=/etc/postgresql/$PG/main/postgresql.conf" <<< "CREATE DATABASE app WITH OWNER app ENCODING 'UTF8';"
 fi
 
