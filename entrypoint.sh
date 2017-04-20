d6ac626ee80bd068dfa1f464e9eb8a224616e46b
Ayhan Akilli <info@ayhan.biz>
2017-04-20 19:02:44 +0200

    code style

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ chown -R app:app $PGDATA /run/postgresql /var/log/postgresql
 if [ -z "$(ls -A $PGDATA)" ]; then
     su -c "initdb -E UTF8 -U app" app
     echo "listen_addresses='*'" >> $PGDATA/postgresql.conf
-    cat << EOF > $PGDATA/pg_hba.conf
+    cat > $PGDATA/pg_hba.conf << EOF
 local   all             all                                     peer
 host    all             all             127.0.0.1/32            md5
 host    all             all             ::1/128                 md5
